0f17347b4ea68ade006f35778a4dd86d25cddb3b
CHERVET Guillaume <guillaume.chervet@gmail.com>
2018-04-18 13:45:17 +0200

    maj

--- introduction/ImperativeProgrammingTest.cs
+++ introduction/ImperativeProgrammingTest.cs
@@ -1,3 +1,5 @@
+#define MYTEST
+
 using System;
 using Xunit;
 
@@ -5,13 +7,13 @@ namespace introduction
 {
     public class ImperativeProgrammingTest
     {
-        public int CalculerMontant(LigneArticle[] ligneArticles)
+        public decimal CalculerMontant(PanierLigneArticle[] panierLigneArticles)
         {
             var montantTotal = 0;
-            for (var i = 0; i < ligneArticles.Length; i++)
+            for (var i = 0; i < panierLigneArticles.Length; i++)
             {
-                var ligneArticle = ligneArticles[i];
-                var article = RecupererArticle(ligneArticle.Id);
+                var ligneArticle = panierLigneArticles[i];
+                var article = GetArticleFromDatabase(ligneArticle.Id);
                 switch (article.Type)
                 {
                     case "banana":
@@ -26,22 +28,27 @@ namespace introduction
                 }
             }
 
-            return montantTotal;
+            return montantTotal / 100;
         }
 
-        public Article RecupererArticle(string id)
+        public ArticleDatabase GetArticleFromDatabase(string id)
         {
+            #if MYTEST
             switch (id)
             {
                 case "1":
-                    return new Article {Id = "1", Price = 1, Stock = 35, Type = "banana"};
+                    return new ArticleDatabase {Id = "1", Price = 1, Stock = 35, Type = "banana"};
                 case "2":
-                    return new Article {Id = "1", Price = 500, Stock = 20, Type = "frigo"};
+                    return new ArticleDatabase {Id = "1", Price = 500, Stock = 20, Type = "frigo"};
                 case "3":
-                    return new Article {Id = "1", Price = 1, Stock = 68, Type = "chaise"};
+                    return new ArticleDatabase {Id = "1", Price = 1, Stock = 68, Type = "chaise"};
                 default:
                     throw new NotImplementedException();
             }
+            #else
+            // TODO request a real database
+            throw new NotImplementedException();
+            #endif
         }
 
         [Fact]
@@ -50,24 +57,24 @@ namespace introduction
             // Récépérer article depuis base de données
             var ligneArticles = new[]
             {
-                new LigneArticle {Id = "1", Number = 12},
-                new LigneArticle {Id = "2", Number = 1},
-                new LigneArticle {Id = "3", Number = 4}
+                new PanierLigneArticle {Id = "1", Number = 12},
+                new PanierLigneArticle {Id = "2", Number = 1},
+                new PanierLigneArticle {Id = "3", Number = 4}
             };
 
             var montantTotal = CalculerMontant(ligneArticles);
 
-            Assert.Equal(montantTotal, 4000);
+            Assert.Equal(montantTotal, 602);
         }
     }
 
-    public struct LigneArticle
+    public struct PanierLigneArticle
     {
         public string Id { get; set; }
         public int Number { get; set; }
     }
 
-    public struct Article
+    public struct ArticleDatabase
     {
         public string Id { get; set; }
         public string Type { get; set; }
