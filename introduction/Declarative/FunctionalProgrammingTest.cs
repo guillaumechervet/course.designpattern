db175cc42fd750b68fbcfe1280e580328b17df14
guillaume chervet <guillaume.chervet@gmail.com>
2018-04-19 10:19:11 +0200

    maj

--- introduction/Declarative/FunctionalProgrammingTest.cs
+++ introduction/Declarative/FunctionalProgrammingTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Immutable;
 using introduction.OrientedObject;
 using introduction.OrientedObject.Infrastructure;
 using Xunit;
@@ -11,48 +12,25 @@ namespace introduction.Declarative
         public void PassingTest()
         {
             // Récépérer article depuis base de données
-            var ligneArticles = new[]
-            {
+        var ligneArticles = ImmutableList.Create(
                 new PanierLigneArticle {Id = "1", Number = 12},
                 new PanierLigneArticle {Id = "2", Number = 1},
                 new PanierLigneArticle {Id = "3", Number = 4}
-            };
+            );
 
-            var montantTotal = CalculerMontantPanier(ligneArticles);
+            var montantTotal = CalculerMontantPanier(GetArticleFromDatabaseMock)(ligneArticles);
     
             Assert.Equal(montantTotal, 60232);
         }
         
-        public decimal CalculerMontantPanier(Func<string, ArticleDatabase> getArticleFromDatabase)
+        public Func<ImmutableList<PanierLigneArticle>, int> CalculerMontantPanier(Func<string, ArticleDatabase> getArticleFromDatabase)
         {
-            
-            var montantTotal = 0;
-            for (var i = 0; i < panierLigneArticles.Length; i++)
-            {
-                var ligneArticle = panierLigneArticles[i];
-                var article = getArticleFromDatabase(ligneArticle.Id);
-                switch (article.Category)
-                {
-                    case "food":
-                        montantTotal += article.Price * 100 + article.Price * 12;
-                        break;
-                    case "electronic":
-                        montantTotal += article.Price * 100 + article.Price * 20 + 4;
-                        break;
-                    case "desktop":
-                        montantTotal += article.Price * 100 + article.Price * 20;
-                        break;
-                }
-            }
-
-            return montantTotal;
-            
-            int Calculer(PanierLigneArticle[] panierLigneArticles)
+            return Calculer;
+            int Calculer(ImmutableList<PanierLigneArticle> panierLigneArticles)
             {
                 var montantTotal = 0;
-                for (var i = 0; i < panierLigneArticles.Length; i++)
+                foreach (var ligneArticle in panierLigneArticles)
                 {
-                    var ligneArticle = panierLigneArticles[i];
                     var article = getArticleFromDatabase(ligneArticle.Id);
                     switch (article.Category)
                     {
@@ -67,9 +45,30 @@ namespace introduction.Declarative
                             break;
                     }
                 }
-
                 return montantTotal;
             }   
+        }
+        
+        public ArticleDatabase GetArticleFromDatabaseMock(string id)
+        {
+            switch (id)
+            {
+                case "1":
+                    return new ArticleDatabase {Id = "1", Price = 1, Stock = 35, Type = "banana", Category = "food"};
+                case "2":
+                    return new ArticleDatabase
+                    {
+                        Id = "1",
+                        Price = 500,
+                        Stock = 20,
+                        Type = "frigo",
+                        Category = "electronic"
+                    };
+                case "3":
+                    return new ArticleDatabase {Id = "1", Price = 1, Stock = 68, Type = "chaise", Category = "desktop"};
+                default:
+                    throw new NotImplementedException();
+            }
         }
 
     }
