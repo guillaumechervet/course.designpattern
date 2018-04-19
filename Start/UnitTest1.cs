3874ada49275ebc81db4b0596d8c2482da5f5c51
CHERVET Guillaume <guillaume.chervet@gmail.com>
2018-04-19 14:05:39 +0200

    maj

---
+++ Start/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Start
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
