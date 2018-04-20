fe316aa51edc371e645ca75b73e78a6a3d02b6dd
CHERVET Guillaume <guillaume.chervet@gmail.com>
2018-04-20 13:40:07 +0200

    maj

---
+++ Paint/src/AvaloniaBeta1DrawingDemo/Models/Square.cs
@@ -0,0 +1,36 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Media;
+
+namespace AvaloniaBeta1DrawingDemo.Models
+{
+    public class Square : Shape
+    {
+        public int Begin {get;set;}
+        public int End {get;set;}
+
+        public Square(Canvas drawingCanvas, Color color) : base(drawingCanvas, color) { }
+
+        public override void Draw()
+        {
+            var square = new Rectangle();
+            var colorBrush = new SolidColorBrush();
+            colorBrush.Color = this.color;
+            square.Fill = colorBrush;
+
+            //square.   
+            square.Width = 400;
+            square.Height = 400;
+            
+             
+            square.Margin = new Thickness(0, 50, 0, 0);
+            //var container = new StackPanel();
+            drawingCanvas.Children.Add(square);
+            Canvas.SetTop(square,2);
+            Canvas.SetLeft(square,2);
+            //this.drawingCanvas.Content = container;
+            //this.drawingCanvas.
+        }
+    }
+}
