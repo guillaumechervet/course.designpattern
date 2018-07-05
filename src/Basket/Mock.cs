4d83e116c151d28221b599a167aa7deba16b22c8
guillaume chervet <guillaume.chervet@gmail.com>
2018-07-05 13:04:54 +0200

    maj

---
+++ src/Basket/Mock.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace BasketLib
+{
+    public class Mock
+    {
+        public static ArticleDatabase GetArticleFromMock(string articleId)
+        {
+            switch (articleId)
+            {
+                case "1":
+                    return new ArticleDatabase() { Category = "food", Price = 1 };
+
+                case "2":
+                    return new ArticleDatabase() { Category = "electronic", Price = 500 };
+
+                case "3":
+                    return new ArticleDatabase() { Category = "desktop", Price = 49 };
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+    }
+}
